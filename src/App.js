def031fce045736b603e1443b9e3d666119acc49
boumessouerabdelkarim <boumessouerabdelkarim@gmail.com>
2022-07-05 11:15:46 +0200

    scond commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import './App.css';
-
+import React from "react";
 
 
 function App() {
   return (
-    <div>
+    <div className='form1' >
     <h1 className='titre'>Login Form</h1>
-    <form className='from1'  >
+    <form className='formcontainer'  >
    
     
     
     <div className="container">
-      <label for="uname"><strong>Username</strong></label>
+      <label  style={{ color: 'rgb(61, 9, 182)'}} htmlFor='uname'><strong>Username</strong></label>
       <input type="text" placeholder="Enter Username" name="uname"className='imput1' required></input>
-      <label for="psw"><strong>Password</strong></label>
-      <input type="password" placeholder="Enter Password" name="psw" required></input>
+      <label  style={{ color: 'rgb(61, 9, 182)'}} htmlFor="psw"><strong>Password</strong></label>
+      <input type="password" placeholder="Enter Password" className='imput1' name="psw" required></input>
     </div>
     <button type="submit" className='login'>Login</button>
     <div className="container" >
-      <label >
-      <input type="checkbox"  checked="checked" name="remember"> Remember me</input>
-      </label>
-      <span className="psw"> Forgot password?</span>
+      
+      <input type="checkbox" name="remember"></input>
+      <label> Remember me</label>
+     
     </div>
   </form>
   </div>
   
   );
-}
+  }
 
 export default App;
